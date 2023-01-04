da1001320a339f8690c6c2d291d2cc64cd147d13
unknown <liancoolbard@gmail.com>
2023-01-04 22:10:51 +0700

    Add apps to the project

--- places_remember/places_remember/settings.py
+++ places_remember/places_remember/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'places_memories',
+    'google_vk_login'
 ]
 
 MIDDLEWARE = [
