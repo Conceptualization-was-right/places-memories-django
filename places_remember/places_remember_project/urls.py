45eed915099a50b43d197c6daff60dbfade48e16
unknown <liancoolbard@gmail.com>
2023-01-06 20:59:52 +0700

    Update project folder name to avoid name conflict

--- places_remember/places_remember_project/urls.py
+++ places_remember/places_remember_project/urls.py
@@ -1,4 +1,4 @@
-"""places_remember URL Configuration
+"""places_remember_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from places_memories import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index)
 ]
